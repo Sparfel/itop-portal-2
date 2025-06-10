ee4291c608c955ce9d6bc38611dcfdd173691089
Emmanuel Lozachmeur <emmanuel.lozachmeur@gmail.com>
2025-06-10 18:13:06 +0200

    - correction du fichier install.sh

--- install.sh
+++ install.sh
@@ -9,11 +9,15 @@ rm -rf public/js public/css
 # Installation des dépendances à la racine
 echo "Installation des dépendances à la racine..."
 npm install
+echo "Exécution de npm audit fix à la racine..."
+npm audit fix
 
 # Installation des dépendances d'AdminLTE
 echo "Installation des dépendances dans resources/vendor/admin-lte..."
 cd resources/vendor/admin-lte
 npm install
+echo "Exécution de npm audit fix pour AdminLTE..."
+npm audit fix
 
 # Compilation des assets d'AdminLTE
 echo "Compilation des assets AdminLTE (npm run production)..."
